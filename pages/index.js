1d8576f51f56006520ea9a4fbd38785e4c5a0d74
Emerson Melo Martins <emerson25xd@gmail.com>
2023-09-20 18:13:52 +0000

    arquivo  formatado

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
-    return (
-        <div>
-            <h1>Novo Commit! 👌🏻</h1>
-            <p>Isso é um teste!</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Novo Commit! 👌🏻</h1>
+      <p>Isso é um teste!</p>
+    </div>
+  );
 }
